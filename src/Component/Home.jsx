15b1ee64cf0c52075fd7cbf16f1a6e369d61cdfa
Abdelrahman Sakr <abdelrahmansakr199@gmail.com>
2025-03-17 12:50:20 +0200

    update-header-home-page

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,11 +1,13 @@
 import axios from "axios";
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useCallback } from "react";
 import { Link } from "react-router-dom";
 import { ThemeContext } from "./Context/GlobalContext";
 
 export default function Home() {
   const [movies, setMovies] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(0);
   const { theme } = useContext(ThemeContext);
+
   useEffect(() => {
     const fetchMovies = async () => {
       try {
@@ -15,64 +17,60 @@ export default function Home() {
         console.error("Error fetching movies:", error);
       }
     };
-
     fetchMovies();
   }, []);
 
-  const images = [
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTotcYeZTbKeKty0BKH-TjNYNDajuJtu_L5fA&s",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5CM8l-PtT0NLKoyNgLZ8prybm0_Uqdub0YA&s",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsMCftGgzk6d4it9Eg_uy3ei-fM1PXri6-yA&s",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2s037hny5bQ2fzim0U2TOrU9DXUGTdP95Xg&s",
-  ];
-
-  const [activeIndex, setActiveIndex] = useState(0);
+  const changeSlide = useCallback(
+    (direction) => {
+      setActiveIndex((prevIndex) =>
+        direction === "next"
+          ? (prevIndex + 1) % movies.length
+          : (prevIndex - 1 + movies.length) % movies.length
+      );
+    },
+    [movies.length]
+  );
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setActiveIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 3000);
-
+    if (movies.length === 0) return;
+    const interval = setInterval(() => changeSlide("next"), 3000);
     return () => clearInterval(interval);
-  }, []);
+  }, [movies, changeSlide]);
 
   return (
     <div
-      className={`max-w-7xl mx-auto px-4 py-5 ${
+      className={`max-w-7xl mx-auto px-4 py-5 items-center ${
         theme === "light" ? "bg-white text-black" : "bg-gray-800 text-white"
       }`}
     >
-      <section className="text-center">
-        <h1 className="border border-gray-800 dark:border-gray-50 p-3 rounded-lg text-5xl font-bold">
+      <section className="text-center relative">
+        <h1 className="border border-gray-800  dark:border-gray-50 p-3 rounded-lg text-5xl font-bold">
           BodyBest Movies
         </h1>
         <hr className="my-4 border-gray-300" />
-
-        <div className="relative w-full max-w-2xl mx-auto overflow-hidden">
+        <div className="relative w-full max-w-2xl mx-auto overflow-hidden border-double rounded-md border-8 border-black">
           <div
             className="flex transition-transform duration-500 ease-in-out"
             style={{ transform: `translateX(-${activeIndex * 100}%)` }}
           >
-            {images.map((src, index) => (
-              <img key={index} src={src} className="w-full flex-shrink-0" />
-            ))}
-          </div>
-
-          <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 flex space-x-2">
-            {images.map((_, index) => (
-              <button
-                key={index}
-                onClick={() => setActiveIndex(index)}
-                className={`w-3 h-3 rounded-full ${
-                  activeIndex === index ? "bg-gray-800" : "bg-gray-400"
-                }`}
-              />
+            {movies.map((movie, index) => (
+              <>
+                <img
+                  key={movie.id}
+                  src={
+                    movie.image?.original ||
+                    "https://via.placeholder.com/210x295?text=No+Image"
+                  }
+                  className="w-full object-fill h-80 flex-shrink-0"
+                  alt="Movie Poster"
+                />
+              </>
             ))}
           </div>
         </div>
       </section>
 
-      <div className="grid grid-cols-2  lg:gap-4 lg:grid-cols-4 md:gap-2 mt-6">
+      <div className="grid grid-cols-2 lg:gap-4 lg:grid-cols-4 md:gap-2 mt-6">
         {movies.map((movie, i) => (
           <div key={i} className="flex justify-start">
             <Link to={`/movieDetails/${movie.id}`}>
--- src/Component/Icon.jsx
+++ src/Component/Icon.jsx
@@ -6,20 +6,20 @@ function Icon() {
     <>
       <div className="d-flex mx-2  align-items-center bg-dark rounded">
         <Link href="https://github.com/Sakr99/BodyBest-Movies.git">
-          <i className="fab mx-2 hover:text-amber-400  fa-github"></i>
+          <i className="fab mx-2 hover:text-gray-400  fa-github"></i>
         </Link>
         <Link href="https://www.facebook.com/">
-          <i className="fab mx-2 hover:text-amber-400 fa-facebook"></i>
+          <i className="fab mx-2 hover:text-gray-400 fa-facebook"></i>
         </Link>
         <Link href="https://www.instagram.com/">
-          <i className="fab mx-2 hover:text-amber-400 fa-instagram"></i>
+          <i className="fab mx-2 hover:text-gray-400 fa-instagram"></i>
         </Link>
         <Link href="https://tiktok.com/">
-          <i className="fab mx-2 hover:text-amber-400 fa-tiktok"></i>
+          <i className="fab mx-2 hover:text-gray-400 fa-tiktok"></i>
         </Link>
 
         <Link href="https://x.com/">
-          <i className="fab mx-2 hover:text-amber-400 fa-x-twitter"></i>
+          <i className="fab mx-2 hover:text-gray-400 fa-x-twitter"></i>
         </Link>
       </div>
     </>
