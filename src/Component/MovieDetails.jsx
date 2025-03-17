93d8734c77273bd317ae74ec6e5e37d4cee7eea0
Abdelrahman Sakr <abdelrahmansakr199@gmail.com>
2025-03-17 13:43:31 +0200

    Update MovieDetails.jsx

--- src/Component/MovieDetails.jsx
+++ src/Component/MovieDetails.jsx
@@ -1,8 +1,9 @@
 import axios from "axios";
 import React, { useState, useEffect ,useContext} from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Video from "../assets/modern-video-player-design-template-260nw-762468289.webp";
 import { ThemeContext } from "./Context/GlobalContext";
+import { data } from "jquery";
 
 export default function MovieDetails() {
   let { id } = useParams();
@@ -30,7 +31,6 @@ export default function MovieDetails() {
             theme === "light" ? " text-black" : "bg-gray-800 "
           }`}
         >
-          {/* Movie Poster & Cast */}
           <div className="md:col-span-1  p-4 rounded-lg shadow-md">
             <img
               className="w-full rounded-lg"
@@ -40,6 +40,17 @@ export default function MovieDetails() {
               }
               alt={movieDetails.name}
             />
+            <h5 className="text-lg font-semibold my-5">Cast:</h5>
+            <ul className="list-disc pl-5 ">
+              <li>George Washington</li>
+              <li>Vin Diesel</li>
+              <li>Tom Cruise</li>
+              <li>Leonardo DiCaprio</li>
+              <li>Robert Downey Jr.</li>
+              <li>Will Smith</li>
+              <li>Brad Pitt</li>
+              <li>Scarlett Johansson</li>
+            </ul>
           </div>
 
           <div className="md:col-span-3 p-6 rounded-lg shadow-md">
@@ -49,23 +60,18 @@ export default function MovieDetails() {
               {movieDetails.rating?.average || "N/A"}
             </div>
             <hr className="my-4" />
-            <p>
+            <p className="my-10">
               {movieDetails.summary?.replace(/<[^>]+>/g, "") ||
                 "No description available."}
             </p>
-            <h5 className="text-lg font-semibold mt-4">Cast:</h5>
-            <ul className="list-disc pl-5 ">
-              <li>Gorg Washnton</li>
-              <li>Van Disil</li>
-              <li>Tom Chros</li>
-              <li>Ahmed Helme</li>
-              <li>Mohamed Ramadan</li>
-              <li>Will Thmes</li>
-              <li>Adel Emam</li>
-              <li>Hend Sabry</li>
-            </ul>
+            <Link
+              to={`https://www.tvmaze.com/shows/${id}/${movieDetails.name.toLowerCase()}`}
+              className="bg-yellow-500 text-white px-8 py-4  font-bold rounded-lg hover:bg-yellow-600 transition"
+            >
+              Watch Now
+            </Link>
 
-            <h4 className="text-xl mt-2 font-semibold mb-2">Movie Story:</h4>
+            <h4 className="text-xl my-10 font-semibold">Movie Story:</h4>
 
             <h4 className="text-xl font-semibold mt-6">Watch the trailer:</h4>
             <img
@@ -74,14 +80,7 @@ export default function MovieDetails() {
               alt="trailer"
             />
 
-            <div className="flex gap-4 mt-4">
-              <button className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 transition">
-                Download
-              </button>
-              <button className="bg-yellow-500 text-white px-4 py-2 rounded-lg hover:bg-yellow-600 transition">
-                Watched
-              </button>
-            </div>
+            <div className="flex gap-4 mt-4"></div>
           </div>
         </div>
       ) : (
